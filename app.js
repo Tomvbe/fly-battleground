0e70eaf3a0b4c4c06300c3757f92d71fffcd110e
Tom Ryzen <tom.vanbesien@gmail.com>
2021-04-24 10:43:55 +0200

    Put everything in functions

--- app.js
+++ app.js
@@ -1,14 +1,73 @@
-const createCanvas = () => {
+function createCanvas() {
     const canvas = document.querySelector("canvas");
     canvas.width = innerWidth;
     canvas.height = innerHeight;
     return canvas;
 }
-const canvas = createCanvas();
 
-const ctx = canvas.getContext('2d');
+function removeFlyFromContext(ctx) {
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+}
+function drawFly(fly, ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = "red"
+    ctx.arc(fly.coordinates.x, fly.coordinates.y, fly.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+}
 
-const fly = {
+function hasContactWithRightWall(fly) {
+    return fly.radius + fly.coordinates.x > innerWidth;
+}
+function hasContactWithLeftWall(fly) {
+    return fly.coordinates.x - fly.radius < 0;
+}
+function invertDirectionX(fly) {
+    fly.velocity.x = 0 - fly.velocity.x;
+}
+
+function hasContactWithCeiling(fly) {
+    return fly.coordinates.y + fly.radius > innerHeight;
+}
+function hasContactWithFloor(fly) {
+    return fly.coordinates.y - fly.radius < 0;
+}
+function invertDirectionY(fly) {
+    fly.velocity.y = 0 - fly.velocity.y;
+}
+
+function moveFlyOneToNextFrame(fly) {
+    fly.coordinates.x = fly.coordinates.x + fly.velocity.x;
+    fly.coordinates.y = fly.coordinates.y + fly.velocity.y;
+}
+
+function bounceFromBorders(fly) {
+    if (hasContactWithRightWall(fly)) {
+        invertDirectionX(fly);
+    }
+
+    if (hasContactWithLeftWall(fly)) {
+        invertDirectionX(fly);
+    }
+
+    if (hasContactWithCeiling(fly)) {
+        invertDirectionY(fly);
+    }
+
+    if (hasContactWithFloor(fly)) {
+        invertDirectionY(fly);
+    }
+}
+
+function move(fly, context) {
+    requestAnimationFrame(() => move(fly, context));
+    removeFlyFromContext(context)
+    drawFly(fly, context);
+    bounceFromBorders(fly);
+    moveFlyOneToNextFrame(fly);
+}
+
+move(
+{
     coordinates: {
         x: Math.floor(Math.random() * innerWidth),
         y: Math.floor(Math.random() * innerHeight),
@@ -18,46 +77,6 @@ const fly = {
         y: Math.floor(Math.random() * 40),
     },
     radius: 30,
-}
-
-const removeBallFromContext = () => ctx.clearRect(0, 0, innerWidth, innerHeight);
-const drawBall = () => {
-    ctx.beginPath();
-    ctx.fillStyle = color
-    ctx.arc(fly.coordinates.x, fly.coordinates.y, fly.radius, 0, Math.PI * 2, false);
-    ctx.fill();
-}
-
-var color = "red"
-const hasContactWithRightWall = (fly) => fly.radius + fly.coordinates.x > innerWidth;
-const hasContactWithLeftWall = (fly) => fly.coordinates.x - fly.radius < 0;
-const invertDirectionX = (fly) => fly.velocity.x = 0 - fly.velocity.x;
-
-function move(fly) {
-
-    requestAnimationFrame(() => move(fly));
-    removeBallFromContext()
-    drawBall();
-
-    if (hasContactWithRightWall(fly)) {
-        invertDirectionX(fly);
-    }
-
-    if (hasContactWithLeftWall(fly)) {
-        invertDirectionX(fly);
-    }
-
-    if (fly.coordinates.y + fly.radius > innerHeight) {
-        fly.velocity.y = 0 - fly.velocity.y;
-    }
-
-    if (fly.coordinates.y - fly.radius < 0) {
-        fly.velocity.y = 0 - fly.velocity.y;
-    }
-
-    fly.coordinates.x = fly.coordinates.x + fly.velocity.x;
-    fly.coordinates.y = fly.coordinates.y + fly.velocity.y;
-
-}
-
-move(fly);
+    },
+    createCanvas().getContext('2d')
+);
