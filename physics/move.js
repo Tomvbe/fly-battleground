4a85db50f2790bcb1bac226d42c8970236c74fb6
Tom Ryzen <tom.vanbesien@gmail.com>
2021-04-24 11:46:13 +0200

    Add new function for single fly movement

--- physics/move.js
+++ physics/move.js
@@ -4,12 +4,13 @@ import {bounceFromBorders} from "./bounceFromBorders.js";
 export function move(flies, context) {
     requestAnimationFrame(() => move(flies, context));
     clearCanvas(context)
+    flies.forEach(fly => moveFly(fly, context))
+}
 
-    flies.forEach(fly => {
-        drawFly(fly, context);
-        bounceFromBorders(fly);
-        moveFlyToNextFrame(fly);
-    })
+function moveFly(fly, context) {
+    drawFly(fly, context);
+    bounceFromBorders(fly);
+    moveFlyToNextFrame(fly);
 }
 
 function moveFlyToNextFrame(fly) {
